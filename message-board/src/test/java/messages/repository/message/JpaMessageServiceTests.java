288bb148c2b43a6a3b156ea00e0440f8fd68a30f
Alexei <Alexei@Alexei-PC>
2012-01-05 21:55:47 +0200

    test commit

--- message-board/src/test/java/messages/repository/message/JpaMessageServiceTests.java
+++ message-board/src/test/java/messages/repository/message/JpaMessageServiceTests.java
@@ -5,6 +5,7 @@ import org.springframework.test.context.ActiveProfiles;
 /**
  * An integration test for {@link JpaMessageService}. Test uses in-memory
  * database.
+ * test test
  */
 @ActiveProfiles(profiles = { "dev", "jpa" })
 public class JpaMessageServiceTests extends MessageServiceTests {
