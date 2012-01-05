be57ea625e04754e5db4c656cccac572dfea3528
Alexei <Alexei@Alexei-PC>
2012-01-05 21:57:07 +0200

    test commit

--- message-board/src/test/java/messages/repository/message/HibernateMessageServiceTests.java
+++ message-board/src/test/java/messages/repository/message/HibernateMessageServiceTests.java
@@ -5,7 +5,6 @@ import org.springframework.test.context.ActiveProfiles;
 /**
  * An integration test for {@link HibernateMessageService}. Test uses in-memory
  * database.
- * test test
  */
 @ActiveProfiles(profiles = { "dev", "hibernate" })
 public class HibernateMessageServiceTests extends MessageServiceTests {
--- message-board/src/test/java/messages/repository/message/JpaMessageServiceTests.java
+++ message-board/src/test/java/messages/repository/message/JpaMessageServiceTests.java
@@ -5,7 +5,6 @@ import org.springframework.test.context.ActiveProfiles;
 /**
  * An integration test for {@link JpaMessageService}. Test uses in-memory
  * database.
- * test test
  */
 @ActiveProfiles(profiles = { "dev", "jpa" })
 public class JpaMessageServiceTests extends MessageServiceTests {
